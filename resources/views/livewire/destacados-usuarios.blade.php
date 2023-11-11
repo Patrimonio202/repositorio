e34ac699b62b33f3769a7ca8ff9102bc696c29db
Patrimonio202 <149069682+Patrimonio202@users.noreply.github.com>
2023-11-10 21:48:48 -0500

    update compartier

--- resources/views/livewire/destacados-usuarios.blade.php
+++ resources/views/livewire/destacados-usuarios.blade.php
@@ -40,9 +40,8 @@
                                     {{ Storage::url($post->image->url) }}
                                 @stop
 
-                                @section('ogTitle')
-                                   Leo el mejor
-                                @stop
+                                @section('ogTitle', 'leo el mejor')
+                                   
                                 <a href="{{ route('posts.show', $post) }}">
                                     <div class="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out"
                                         style="background-color: rgba(251, 251, 251, 0.15)"></div>
