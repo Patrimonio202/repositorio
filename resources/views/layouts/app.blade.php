b7ae08b5cbc05b56322217b5e6f26f2aa109d49f
Patrimonio202 <149069682+Patrimonio202@users.noreply.github.com>
2023-11-14 17:05:01 -0500

    Prueba banner

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,9 +76,10 @@
     <body class="font-sans antialiased">
         <x-banner />
 
-        <div class="min-h-screen flex flex-col sm:justify-center bg-gray-100">          
+        <div class="min-h-screen flex flex-col sm:justify-center bg-gray-100">      
+          <div class="container my-4 md:my-4 lg:my-4 px-6 mx-auto ">    
              @livewire('navigation')           <!-- este es el menu que acabamos de crear -->    
-        
+          </div>
             <!-- Page Content -->
             <main>
                 {{ $slot }}
