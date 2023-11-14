2fa0be7f0e611477f7ddb1061e646bb0edbea21b
Patrimonio202 <149069682+Patrimonio202@users.noreply.github.com>
2023-11-14 17:28:39 -0500

    cuadre logo

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ Storage::url('Imagenes/Logo.png') }}" class="w-50 h-10 rounded-lg" alt="...">
+<img src="{{ Storage::url('Imagenes/Logo.png') }}" class=" w-20 h-10 lg:w-52 lg:h-10 rounded-lg" alt="...">
