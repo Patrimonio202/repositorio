7cf3df32e7a51b3d2041935d13bb5d10c6959e0b
Patrimonio202 <149069682+Patrimonio202@users.noreply.github.com>
2023-11-15 06:58:07 -0500

    Intervalo de tiempo slider

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -14,64 +14,62 @@
 
         </section> max-w-8xl
     </div> --}}-->
-    <div class="grid grid-cols-1    lg:flex max-w-7xl lg:mx-auto mx-4   bg-white rounded-lg  shadow-lg   text-center py-8 px-6 gap-4 mt-8 ">
-       
-           
-                
+    <div
+        class="grid grid-cols-1    lg:flex max-w-7xl lg:mx-auto mx-4   bg-white rounded-lg  shadow-lg   text-center py-8 px-6 gap-4 mt-8 ">
 
-                    <div >
-                        <div class="mb-2 lg:mb-4">
-                            <a href="{{ route('posts.buscar') . '?category%5B%5D=1' }}">
-                                <img src="{{ Storage::url('Imagenes/Imagenes.jpg') }}"
-                                    class="inline-block w-10 h-10 ml-4 rounded-xl hover:scale-150 transition-all duration-100"
-                                    alt="...">
-                            </a>
+        <div>
+            <div class="mb-2 lg:mb-4">
+                <a href="{{ route('posts.buscar') . '?category%5B%5D=1' }}">
+                    <img src="{{ Storage::url('Imagenes/Imagenes.jpg') }}"
+                        class="inline-block w-10 h-10 ml-4 rounded-xl hover:scale-150 transition-all duration-100"
+                        alt="...">
+                </a>
 
-                            <a href="{{ route('posts.buscar') . '?category%5B%5D=3' }}">
-                                <img src="{{ Storage::url('Imagenes/Libros.jpg') }}"
-                                    class="inline-block h-12 w-10 object-cover object-center ml-4 rounded-xl hover:scale-150 transition-all duration-100"
-                                    alt="...">
-                            </a>
-                            <a href="{{ route('posts.buscar') . '?category%5B%5D=4' }}">
-                                <img src="{{ Storage::url('Imagenes/Videos.jpg') }}"
-                                    class="inline-block h-10 w-10 object-cover object-center ml-4 rounded-xl hover:scale-150 transition-all duration-100"
-                                    alt="...">
-                            </a>
-                            <a href="{{ route('posts.buscar') . '?category%5B%5D=2' }}">
-                                <img src="{{ Storage::url('Imagenes/Audios.jpg') }}"
-                                    class="inline-block h-12 w-10 object-cover object-center ml-4 rounded-xl hover:scale-150 transition-all duration-100"
-                                    alt="...">
-                            </a>
-                        </div>
+                <a href="{{ route('posts.buscar') . '?category%5B%5D=3' }}">
+                    <img src="{{ Storage::url('Imagenes/Libros.jpg') }}"
+                        class="inline-block h-12 w-10 object-cover object-center ml-4 rounded-xl hover:scale-150 transition-all duration-100"
+                        alt="...">
+                </a>
+                <a href="{{ route('posts.buscar') . '?category%5B%5D=4' }}">
+                    <img src="{{ Storage::url('Imagenes/Videos.jpg') }}"
+                        class="inline-block h-10 w-10 object-cover object-center ml-4 rounded-xl hover:scale-150 transition-all duration-100"
+                        alt="...">
+                </a>
+                <a href="{{ route('posts.buscar') . '?category%5B%5D=2' }}">
+                    <img src="{{ Storage::url('Imagenes/Audios.jpg') }}"
+                        class="inline-block h-12 w-10 object-cover object-center ml-4 rounded-xl hover:scale-150 transition-all duration-100"
+                        alt="...">
+                </a>
+            </div>
 
 
-                        
-                    </div>
 
-                     <div class=" md:flex-1 lg:flex-1 pl-0  ml-0">
-                        <label for="default-search"
-                            class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Buscar</label>
-                        <div class="relative">
-                            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                                </svg>
-                            </div>
-                            <input type="search" id="default-search"
-                                class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="Buscar imagenes, libros, audios, videos..." required>
-                            <button type="submit"
-                                class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Buscar</button>
-                        </div>
-                    </div> 
+        </div>
+
+        <div class=" md:flex-1 lg:flex-1 pl-0  ml-0">
+            <label for="default-search"
+                class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Buscar</label>
+            <div class="relative">
+                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                    </svg>
+                </div>
+                <input type="search" id="default-search"
+                    class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Buscar imagenes, libros, audios, videos..." required>
+                <button type="submit"
+                    class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Buscar</button>
+            </div>
+        </div>
 
 
-                
-             
-    
-     
+
+
+
+
     </div>
 
 
