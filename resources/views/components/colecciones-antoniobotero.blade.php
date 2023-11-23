0dfe43de9d038b0c1d7922618f730fa9e0435514
Patrimonio202 <149069682+Patrimonio202@users.noreply.github.com>
2023-11-22 22:27:22 -0500

    Organizando font

---
+++ resources/views/components/colecciones-antoniobotero.blade.php
@@ -0,0 +1,106 @@
+<div class="flex mt-4 items-center justify-center text-center  bg-center  ">
+    <img src="{{ Storage::url('Imagenes/Antonio_botero_titulo.png') }}" class=" object-cover object-center"
+        alt="Titulo Antonio botero">
+
+</div>
+<div class="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-2  mt-16 items-center justify-center bg-center text-center">
+    <div class=" mt-14  mr-20">
+        <h1 class=" text-center  text-[#08416b]" style="font-family:Raleway-ExtraBold">CELBRANDO EL PATRIMONIO
+            FOTOGRÁFICO DE DON
+            ANTONIO BOTERO GÓMEZ (1919-2000)</h1>
+        <div class=" text-left text-sm mt-4 text-[#08416b]" style="font-family:Raleway-Regular">
+            <p>La fotografía es un poderoso medio para capturar la esencia de una época, y el legado de Don
+                Antonio
+                Botero Gómez es un testimonio invaluable <span class="font-bold">de la vida y la cultura en El
+                    Santuario</span>, a lo largo de
+                varias
+                décadas del siglo XX. Esta exhibición y la colección que está disponible en línea a través del
+                repositorio digital de archivos patrimoniales de la Secretaría de Cultura de El Santuario
+                <span class="font-bold">www.patrimonioelsantuario.gov.co</span>, rinde homenaje a la pasión y el arte de
+                un fotógrafo
+                autodidacta cuyo
+                trabajo se ha convertido en un patrimonio visual inestimable.
+            </p>
+            <br>
+            <p>
+                La historia de Don Antonio Botero Gómez como fotógrafo comenzó bajo la tutela del distinguido fotógrafo
+                de Medellín de mediados del siglo pasado, Francisco Mejía. Esta influencia temprana marcó el inicio de
+                una trayectoria dedicada al arte de la fotografía en el municipio de El Santuario. La pasión que sintió
+                por la fotografía y el cine lo acompañaría a lo largo de su vida, y en sus manos, <span
+                    class="font-bold">una cámara se
+                    convertiría en un pincel para retratar la vida en su comunidad.</span>
+            </p>
+        </div>
+    </div>
+    <div class=" items-center justify-center bg-center text-center">
+        <img src="{{ Storage::url('Imagenes/Antonioboterofoto.png') }}" class=" w-96  object-center object-cover"
+            alt="Foto Antonio botero">
+    </div>
+</div>
+
+<div class=" mt-0">
+    <img src="{{ Storage::url('Imagenes/Antonio_botero_coleccion.png') }}" class=" object-cover object-center"
+        alt="Foto Antonio botero">
+</div>
+
+<div>
+    <h1 class=" text-center text-[#08416b]" style="font-family:Raleway-ExtraBold">MÁS ALLÁ DE LA FOTOGRAFÍA,
+        CONSERVANDO UN LEGADO VISUAL</h1>
+    <div class=" text-justify text-sm mt-4 text-[#08416b]" style="font-family:Raleway-Regular">
+        <p >
+            Lo que distingue a Don Antonio Botero Gómez no es solo su habilidad técnica detrás de la cámara, sino
+            también su maestría artística. Sus fotografías no son simples instantáneas, sino verdaderas obras de
+            arte. Utilizaba su conocimiento químico para revelar y, en algunos casos, iluminar o pintar sus
+            imágenes, lo que aportaba un toque distintivo a su trabajo. <span class="font-bold">El inmenso archivo que
+                se ha conservado
+                hasta nuestras épocas ofrece una visión de su creatividad y cómo sus habilidades artísticas elevaron sus
+                fotografías a una expresión única de su visión.</span>  
+        </p>
+        <br>
+        <p>
+            Don Antonio Botero Gómez tenía la costumbre de guardar meticulosamente sus negativos fotográficos. Hoy,
+            gracias a esta práctica, podemos explorar una colección única que abarca décadas y que presenta los
+            rostros, las costumbres, las alegrías y las tristezas de la comunidad santuariana
+        </p>
+    </div>
+</div>
+
+<div class="flex mt-8">
+    <div class=" w-52 mt-10 mr-8">
+        <img src="{{ Storage::url('Imagenes/CamaraAntonioBotero_L.png') }}" class=" object-cover object-center"
+            alt="Foto Antonio botero">
+    </div>
+    <div class=" mt-4 flex-1 ">
+        <h1 class=" text-center text-[#08416b]" style="font-family:Raleway-ExtraBold">
+            EXPLORANDO EL PATRIMONIO VISUAL DE EL SANTUARIO
+        </h1>
+        <div class=" text-justify text-sm mt-4 text-[#08416b]" style="font-family:Raleway-Regular">
+            <p>
+                Esta colección ofrece a los visitantes la oportunidad de sumergirse en la historia de El
+                Santuario a través de los ojos y el corazón de Don Antonio Botero Gómez. Las imágenes capturan
+                momentos cotidianos y excepcionales, desde celebraciones festivas, hasta algunas postales de la
+                vida cotidiana. <span class="font-bold">Cada fotografía es un testimonio de la riqueza de la cultura y la identidad de
+                El Santuario.</span> 
+            </p>
+            <br>
+            <p>
+                El Archivo de Antonio Botero Gómez que está compuesto por <span class="font-bold">30.000 negativos fotográficos</span>, de las
+                cuales han sido digitalizados casi 4.000, es una celebración del legado visual de un apasionado
+                fotógrafo cuyo trabajo trasciende el simple acto de tomar una fotografía.
+            </p>
+            <br>
+            <p>
+                Estas imágenes están disponibles en alta calidad en nuestro repositorio digital para su
+                consulta, estudio, investigación y divulgación, gracias al esfuerzo de varios investigadores,
+                gestores culturales y artistas que desde la Secretaría de Cultura, Patrimonio y Turismo y con el
+                respaldo de varias instituciones del nivel nacional vienen abordando esta colección para ponerla
+                en valor y que tenga la mayor vigencia en nuestros días.
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class=" mb-8 mt-8">
+    <img src="{{ Storage::url('Imagenes/Antonio_botero_frasefinal.png') }}" class=" object-cover object-center"
+        alt="Frase final Antonio botero">
+</div>
