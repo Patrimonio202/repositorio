303bacf32ec338fc8c23226640b08eb869d50e02
Patrimonio202 <149069682+Patrimonio202@users.noreply.github.com>
2023-12-01 22:08:55 -0500

    Organizar visor de pdf

--- resources/views/livewire/showlw.blade.php
+++ resources/views/livewire/showlw.blade.php
@@ -364,11 +364,11 @@
                         {{-- <iframe width="100%" height="700"
                         src="{{ Storage::url($post->image->urlarchivo) }}#toolbar=0&navpanes=0&scrollbar=0">
                         </iframe> --}}
-                        <object data="{{ Storage::url($post->image->urlarchivo) }}" type="application/pdf"
-                            frameborder="0" width="100%" height="600px" style="padding: 20px;">
-                            {{-- <embed src="{{ Storage::url($post->image->urlarchivo) }}" width="100%" height="600px"/>  --}}
+                        {{-- <object data="{{ Storage::url($post->image->urlarchivo) }}" type="application/pdf"
+                            frameborder="0" width="100%" height="600px" style="padding: 20px;">                          
                                 <iframe class=" w-full" height="600px" src="pdfjs/web/viewer.html?file={{ Storage::url($post->image->urlarchivo) }}" frameborder="0"></iframe>
-                        </object>
+                        </object> --}}
+                         <iframe class=" w-full" height="600px" src="{{ asset('pdfjs/web/viewer.html')}}?file={{ Storage::url($post->image->urlarchivo) }}" frameborder="0"></iframe>
 
                     </div>
 
