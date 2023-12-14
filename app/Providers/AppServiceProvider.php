8da7a3abaa5d62306e02f99d823fe96286c20ec1
Patrimonio202 <149069682+Patrimonio202@users.noreply.github.com>
2023-12-14 01:00:45 -0500

    dsdsd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,9 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_ENV') !== 'local') {
-            $this->app['request']->server->set('HTTPS', 'on');
-            URL::forceScheme('https');
-        }
+        // if (env('APP_ENV') !== 'local') {
+        //     $this->app['request']->server->set('HTTPS', 'on');
+        //     URL::forceScheme('https');
+        // }
     }
 }
