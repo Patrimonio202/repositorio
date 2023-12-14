d937a7ad32a9f64e4096bc956ce1e11e9c349e41
Patrimonio202 <149069682+Patrimonio202@users.noreply.github.com>
2023-12-13 23:38:45 -0500

    Corregir error de nombre

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -30,14 +30,16 @@ class WelcomeController extends Controller
 
         foreach ($posts as $post) {
             if ($post->category_id == '1' ||  $post->category_id == '2' || $post->category_id == '3') {
-                $fileName = $post->image->url;
-                //dd($fileName);
-                $public = Storage::disk('public');
-                $s3 = Storage::disk('s3');
-                $public->writeStream($fileName, $s3->readStream($fileName));
+                if ($post->image->url) {
+                    $fileName = $post->image->url;
+                    //dd($fileName);
+                    $public = Storage::disk('public');
+                    $s3 = Storage::disk('s3');
+                    $public->writeStream($fileName, $s3->readStream($fileName));
+                }
             }
 
-            if ( $post->category_id == '2' || $post->category_id == '3') {
+            if ($post->category_id == '2' || $post->category_id == '3') {
                 $fileName = $post->image->urlarchivo;
                 //dd($fileName);
                 $public = Storage::disk('public');
