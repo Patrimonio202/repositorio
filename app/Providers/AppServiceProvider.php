f17b4e05cbb2a126d5399f927bc3d76400329258
Patrimonio202 <149069682+Patrimonio202@users.noreply.github.com>
2023-12-27 20:24:22 -0500

    forzar https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,8 +22,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // if (env('APP_ENV') !== 'local') {
-        //     $this->app['request']->server->set('HTTPS', 'on');
-        //     URL::forceScheme('https');
+             $this->app['request']->server->set('HTTPS', 'on');
+             URL::forceScheme('https');
         // }
 
 
